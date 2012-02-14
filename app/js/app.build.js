c5d01b58d325fe40e7d5102da9bafd17c29cdeb4
Eric Matthys <eric@realeyes.com>
2012-02-14 11:28:23 -0700

    renamed libraries to include amd prefix

--- app/js/app.build.js
+++ app/js/app.build.js
@@ -4,8 +4,8 @@
 	dir: '../../build',
 	paths: {
 		jquery: 'libs/jquery',
-		underscore: 'libs/underscore-1.3.1-min',
-		backbone: 'libs/backbone-0.9.1-min',
+		underscore: 'libs/amd-underscore-1.3.1-min',
+		backbone: 'libs/amd-backbone-0.9.1-min',
 		templates: '../templates'
 	},
 	modules: [
--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ require.config({
 	//urlArgs: 'dc=' + (new Date()).getTime(),
 	paths: {
 		jquery: 'libs/jquery',
-		underscore: 'libs/underscore-1.3.1-min',
-		backbone: 'libs/backbone-0.9.1-min',
+		underscore: 'libs/amd-underscore-1.3.1-min',
+		backbone: 'libs/amd-backbone-0.9.1-min',
 		templates: '../templates'
 	}
 });
